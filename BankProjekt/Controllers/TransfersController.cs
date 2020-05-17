b1ef37a14bbdfb2aaada27e64b6dd4715af71106
sccamati <64529630+sccamati@users.noreply.github.com>
2020-05-17 19:23:25 +0200

    Projekt

--- BankProjekt/Controllers/TransfersController.cs
+++ BankProjekt/Controllers/TransfersController.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using BankProjekt.DAL;
+using BankProjekt.Models;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using BankProjekt.DAL;
-using BankProjekt.Models;
+using System.Web;
 
 
 namespace BankProjekt.Controllers
@@ -17,33 +18,52 @@ namespace BankProjekt.Controllers
         private BankContext db = new BankContext();
 
         // GET: Transfers
-        public ActionResult Index(int? id, string sortOrder, string searchString)
+        public ActionResult Index(int? id, string sortOrder, string searchString, int? page, string currentFilter)
         {
             var bankAccounts = db.Profiles.Single(u => u.Email == User.Identity.Name).BankAccounts;
             var transfers = db.Transfers.Select(t => t);
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewBag.CashSortParam = sortOrder == "Cash" ? "cash_desc" : "Cash";
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
 
             if (!String.IsNullOrEmpty(searchString))
             {
                 transfers = transfers.Where(s => s.Title.Contains(searchString));
             }
 
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
-            ViewBag.CashSortParam = sortOrder == "Cash" ? "cash_desc" : "Cash";
             switch (sortOrder)
             {
-
                 case "Date":
                     transfers = transfers.OrderBy(s => s.Date);
                     break;
+
                 case "date_desc":
                     transfers = transfers.OrderByDescending(s => s.Date);
                     break;
+
                 case "Cash":
                     transfers = transfers.OrderBy(s => s.Cash);
                     break;
+
                 case "cash_desc":
                     transfers = transfers.OrderByDescending(s => s.Cash);
                     break;
+
                 default:
                     transfers = transfers.OrderByDescending(s => s.Date);
                     break;
@@ -51,28 +71,23 @@ namespace BankProjekt.Controllers
 
             if (id == null)
             {
-                
                 List<Transfer> list = new List<Transfer>();
 
                 foreach (var item in bankAccounts)
                 {
-
-                    list.AddRange(transfers.Where(t => t.AddressesNumber.Equals(item.Number) || t.ReceiversNumber.Equals(item.Number))); 
+                    list.AddRange(transfers.Where(t => t.AddressesNumber.Equals(item.Number) || t.ReceiversNumber.Equals(item.Number)));
                 }
                 list.OrderByDescending(t => t.Date);
-                
-                return View(list);
+
+                return View(list.ToPagedList(pageNumber, pageSize));
             }
             else
             {
                 var bankAccountsNumber = bankAccounts.Single(b => b.Id.Equals(id)).Number;
                 transfers = transfers.Where(t => t.AddressesNumber.Equals(bankAccountsNumber) || t.ReceiversNumber.Equals(bankAccountsNumber));
 
-                return View(transfers.ToList());
+                return View(transfers.ToPagedList(pageNumber, pageSize));
             }
-            
-           
-            
         }
 
         // GET: Transfers/Details/5
@@ -97,7 +112,7 @@ namespace BankProjekt.Controllers
         }
 
         // POST: Transfers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -129,7 +144,7 @@ namespace BankProjekt.Controllers
         }
 
         // POST: Transfers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -177,6 +192,19 @@ namespace BankProjekt.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<TransferDateGroup> data = db.Transfers.GroupBy(t => t.Date).Select( t => new TransferDateGroup
+            {
+                Date = t.Key,
+                TransferCount = t.Count()
+            });
+
+      
+
+            return View(data.ToList());
         }
     }
 }
--- BankProjekt/Models/Transfer.cs
+++ BankProjekt/Models/Transfer.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BankProjekt.Models
 {
+    [Authorize(Roles = "Admin, User")]
     public class Transfer
     {
         public int Id { get; set; }
